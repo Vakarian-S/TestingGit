f8071cad16eb6b1499bdbd23986b481322f35895
Sebastian Villarroel <sebastian.villarroel.ucn@gmail.com>
2018-12-29 22:47:59 -0300

    Tula

---
+++ codigo/proyecto/app/ActividadExtensionOrador.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActividadExtensionOrador extends Model
+{
+    protected $fillable = [
+        'actividad_extension_id', 'orador'
+    ]; //
+    //
+
+
+}
---
+++ codigo/proyecto/app/ActividadExtensionOrganizador.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActividadExtensionOrganizador extends Model
+{
+    protected $fillable = [
+        'actividad_extension_id', 'organizador'
+    ]; //
+}
