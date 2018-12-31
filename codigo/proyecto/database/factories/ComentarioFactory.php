77d1aa68c1c737e91a4a1ff343beefdded203b1d
Sebastian Villarroel <sebastian.villarroel.ucn@gmail.com>
2018-12-31 17:55:16 -0300

    Implementacion de Confirmacion via Bootstrap Modal, Validacion usando el sistema de validaciones de Laravel y poblado de selecciones en Drop/down list de convenios al momento de cargar registro Extension

--- codigo/proyecto/database/factories/ComentarioFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Comentario::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
--- codigo/proyecto/database/factories/PruebaFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Faker\Generator as Faker;
-
-$factory->define(App\Prueba::class, function (Faker $faker) {
-    return [
-        //
-    ];
-});
